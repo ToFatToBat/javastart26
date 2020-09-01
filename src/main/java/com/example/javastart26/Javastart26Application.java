77a40fde0efcdac7a6ea170b2618982fd3e33512
ToFatToBat <kpapiernia@wp.pl>
2020-09-01 20:09:05 +0200

    Initial commit

---
+++ src/main/java/com/example/javastart26/Javastart26Application.java
@@ -0,0 +1,13 @@
+package com.example.javastart26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javastart26Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javastart26Application.class, args);
+    }
+
+}
