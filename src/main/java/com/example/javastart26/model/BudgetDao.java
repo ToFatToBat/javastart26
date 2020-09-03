244822306b30d1a42a252619c61d5254dfe80fa7
ToFatToBat <kpapiernia@wp.pl>
2020-09-03 13:30:46 +0200

    BudgetDao - add method

--- src/main/java/com/example/javastart26/model/BudgetDao.java
+++ src/main/java/com/example/javastart26/model/BudgetDao.java
@@ -2,6 +2,7 @@ package com.example.javastart26.model;
 
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class BudgetDao {
@@ -18,5 +19,18 @@ public class BudgetDao {
             e.printStackTrace();
         }
     }
+
+    public void add(Budget budget) {
+        final String sql = "insert into home_budget (iotype, description, amount) values(???)";
+        try{
+            PreparedStatement prepStmt = connection.prepareStatement(sql);
+            prepStmt.setString(2, budget.getiOType() );
+            prepStmt.setString(3, budget.getDescription() );
+            prepStmt.setDouble(4, budget.getAmount() );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
