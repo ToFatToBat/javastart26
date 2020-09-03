9882e6c684d0d96595dbcd8d85cdde6d80008f95
ToFatToBat <kpapiernia@wp.pl>
2020-09-03 13:11:56 +0200

    BudgetDao added

--- src/main/java/com/example/javastart26/model/BudgetDao.java
+++ src/main/java/com/example/javastart26/model/BudgetDao.java
@@ -1,6 +1,22 @@
 package com.example.javastart26.model;
 
-public class BudgetDao {
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+public class BudgetDao {
+    private static final String URL = "jdbc:mysql://localhost:3306/budget";
+    private static final String USER = "root";
+    private static final String PASS = "hockiklockikotfilemon";
+    private Connection connection;
+
+
+    public BudgetDao() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
