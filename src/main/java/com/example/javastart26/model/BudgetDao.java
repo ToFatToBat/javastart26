f813793234006ecb7699b138391afa37311a5844
ToFatToBat <kpapiernia@wp.pl>
2020-09-05 10:31:03 +0200

    BudgetDao - update / delete method added

--- src/main/java/com/example/javastart26/model/BudgetDao.java
+++ src/main/java/com/example/javastart26/model/BudgetDao.java
@@ -2,6 +2,7 @@ package com.example.javastart26.model;
 
 import java.net.URL;
 import java.sql.*;
+import java.util.EmptyStackException;
 
 public class BudgetDao<connection> {
     private static final String URL = "jdbc:mysql://localhost:3306/budget?characterEncoding=utf8&serverTimezone=UTC&useSSL=false";
@@ -9,25 +10,25 @@ public class BudgetDao<connection> {
     private static final String PASS = "root";
     Connection connection;
 
-    public BudgetDao(){
+    public BudgetDao() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection(URL, USER, PASS);
         } catch (ClassNotFoundException e) {
             System.out.println("No driver found");
             e.printStackTrace();
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println("Could not establish connection");
         }
     }
 
-    public void save (Budget budget) {
-        final String sql = "insert into home_budget (iotype, description, amount) values(???)";
-        try{
+    public void save(Budget budget) {
+        final String sql = "insert into home_budget (iotype, description, amount) values(?,?,?)";
+        try {
             PreparedStatement prepStmt = connection.prepareStatement(sql);
-            prepStmt.setString(2, budget.getiOType() );
-            prepStmt.setString(3, budget.getDescription() );
-            prepStmt.setDouble(4, budget.getAmount() );
+            prepStmt.setString(1, budget.getiOType());
+            prepStmt.setString(2, budget.getDescription());
+            prepStmt.setDouble(3, budget.getAmount());
             prepStmt.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Could not save record");
@@ -35,9 +36,9 @@ public class BudgetDao<connection> {
         }
     }
 
-    public void get (long id) {
+    public Budget get(long id) {
         final String sql = "select id, iotype, description, amount from home_budget where id = ?";
-        try{
+        try {
             PreparedStatement prepStmt = connection.prepareStatement(sql);
             prepStmt.setLong(1, id);
             ResultSet resultSet = prepStmt.executeQuery();
@@ -47,10 +48,38 @@ public class BudgetDao<connection> {
                 budget.setiOType(resultSet.getString("iotype"));
                 budget.setDescription(resultSet.getString("description"));
                 budget.setAmount(resultSet.getDouble("amount"));
+                return budget;
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return null;
     }
 
+    public void update(Budget budget) {
+        final String sql = "update home_budget set id = ?, iotype = ?, description = ?, amount = ?";
+        try {
+            PreparedStatement prepStmt = connection.prepareStatement(sql);
+            prepStmt.setLong(1, budget.getId());
+            prepStmt.setString(2, budget.getiOType());
+            prepStmt.setString(3, budget.getDescription());
+            prepStmt.setDouble(3, budget.getAmount());
+            prepStmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Could not update record");
+            e.printStackTrace();
+        }
+    }
+
+    public void delete (long id) {
+        final String sql = "delete from home_budget where id = ?";
+        try {
+            PreparedStatement prepStmt = connection.prepareStatement(sql);
+            prepStmt.setLong(1, id);
+            prepStmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Could not delete row");
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/example/javastart26/model/main.java
+++ src/main/java/com/example/javastart26/model/main.java
@@ -5,6 +5,17 @@ import java.sql.SQLException;
 public class main {
     public static void main(String[] args) throws SQLException {
         BudgetDao budgetDao = new BudgetDao();
-        budgetDao.add(new Budget("in", "koks", 25000));
+        budgetDao.save(new Budget("in", "koks", 25000));
+        budgetDao.save(new Budget("in", "wegiel", 11000));
+        budgetDao.save(new Budget("in", "piasek", 9000));
+        budgetDao.save(new Budget("out", "koks", 15000));
+        budgetDao.save(new Budget("out", "piasek", 4000));
+
+        System.out.println(budgetDao.get(1));
+
+
+
+
+
     }
 }
